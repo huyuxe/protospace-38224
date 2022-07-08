0859964e08f0e32071f85e69a2209a96bd19e4b3
huyuxe <107824613+huyuxe@users.noreply.github.com>
2022-07-08 12:26:23 +0900

    編集した時にデータが残るよう修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,12 +29,12 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-   
+
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
